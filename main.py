9ad02dcb2a447513cbff6303faee5f83da06c075
nazish-qamar <64389147+nazish-qamar@users.noreply.github.com>
2022-06-04 19:15:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from fetch_reviews import get_review
+from fetch_reviews import get_review, get_available_files
 from sentiment_prediction import get_sentiment_overview
+import streamlit as st
 
 if __name__ == '__main__':
-    url = "https://www.amazon.com/Aiw-Wfdnn-Beanie-Trust-DeepHeather/dp/B07HY25PQ6/ref=sr_1_2?keywords=gaming%2Bhats&pd_rd_r=a37e132e-358f-4645-84d4-f0e8532384d4&pd_rd_w=bIa6V&pd_rd_wg=rHpMJ&pf_rd_p=09483392-9ac6-434a-a3d7-39d83662f54a&pf_rd_r=X6PC9DZEVSHW1XB0Y12J&qid=1653944503&sr=8-2&th=1&psc=1"
-    url_review = url.split("/")[0] + "/" + \
-                 url.split("/")[1] + "/" + \
-                 url.split("/")[2] + "/" + \
-                 url.split("/")[3] + "/" + \
-                 "product-reviews/" + \
-                 url.split("/")[5] + "/" + "ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews"
-    get_review(url_review)
+    st.title("Amazon Reviews Sentiment")
+    choice = st.selectbox("Select Action", ("Scrape Reviews", "Predict Sentiment"))
 
-    get_sentiment_overview()
+    if "Scrape Reviews" == choice:
+        st.write("Please copy the complete URL of the product page and paste below!")
+        url = st.text_input("Product URL")
+        if url:
+            get_review(url)
+            st.write("Done!")
+
+    else:
+        files = get_available_files()
+        file_choice = st.selectbox("Choose the review file for sentiment overview", files)
+
+        button_choice = st.button('Predict Review Sentiment!')
+        if button_choice:
+            with st.spinner('Wait for it...'):
+                positive_percentage = get_sentiment_overview(file_choice)
+                st.write(positive_percentage)
